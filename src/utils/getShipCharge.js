7af5c0a12d8ace9234b676faa9b8403c8380425e
devprabhsingh <iprabhsingh0@gmail.com>
2025-07-08 23:12:49 -0400

    v1.82

--- src/utils/getShipCharge.js
+++ src/utils/getShipCharge.js
@@ -1,4 +1,4 @@
-import categories from "./data/categories";
+import categories from "../data/categories";
 
 function getPrice(distance, subtotal, cartItems) {
   const items = cartItems.map((cItem) => {
