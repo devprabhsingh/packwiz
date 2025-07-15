5afd65f74c3d44af579d3cabce37e1019396db52
devprabhsingh <iprabhsingh0@gmail.com>
2025-07-15 19:30:37 -0400

    v12

--- src/data/discountCodes.js
+++ src/data/discountCodes.js
@@ -1,6 +1,7 @@
 const discountCodes = [
   { code: "LLANA10", type: "percent", amount: 10 },
   { code: "WELCOME10", type: "percent", amount: 10 },
+  { code: "JAVAID25", type: "percent", amount: 25 },
 ];
 
 export default discountCodes;
